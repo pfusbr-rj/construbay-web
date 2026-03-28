69951848eb388147f7ec68b20e8a48d362273726
pfusbr-rj <pf.us.br@gmail.com>
2026-03-28 10:14:20 -0700

    install Microsoft Clarity w2xbo5ao6o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -196,7 +196,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "CLARITY_PROJECT_ID");
+          })(window, document, "clarity", "script", "w2xbo5ao6o");
         `}
       </Script>
       <body
