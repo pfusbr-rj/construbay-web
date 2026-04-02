9a5df2344c24481e9ec82bc364ed1a52fe8b6d79
pfusbr-rj <pf.us.br@gmail.com>
2026-04-01 19:20:07 -0700

    config: allow images.unsplash.com as remote image source

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   staticPageGenerationTimeout: 300,
   experimental: {
     workerThreads: false,
